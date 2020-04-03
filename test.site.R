d92e171ad5f901a5175257c062221ab432ec33e1
chengkeren <inowkeren@gmail.com>
2020-04-03 02:25:42 -0500

    test

--- test.site.R
+++ test.site.R
@@ -1,6 +1,6 @@
 install.packages('blogdown')
 
 getwd()
-setwd("/Volumes/Seagate/Github/test")
+setwd("/Volumes/Seagate/Github/chengkeren.github.io")
 # for example, create a new site with the academic theme
 blogdown::new_site(theme = 'gcushen/hugo-academic')
