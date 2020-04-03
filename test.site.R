508434ec9441bd99a6d4619c95157d7a05f4393f
chengkeren <inowkeren@gmail.com>
2020-04-03 02:37:17 -0500

    KC

--- test.site.R
+++ test.site.R
@@ -3,4 +3,6 @@ install.packages('blogdown')
 getwd()
 setwd("/Volumes/Seagate/Github/chengkeren.github.io")
 # for example, create a new site with the academic theme
-blogdown::new_site(theme = 'gcushen/hugo-academic')
+blogdown::new_site(theme = 'gcushen/hugo-academic',  force = TRUE)
+# 改完所有的Rmd之后，build
+blogdown::build_dir(dir = "/Volumes/Seagate/Github/chengkeren.github.io", force = FALSE, ignore = "[.]Rproj$")
